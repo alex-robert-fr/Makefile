b1bf3801716963bd97225ba31d3917172d5153b5
Alex Robert <contact.alexrobert04@gmail.com>
2023-12-30 17:26:27 +0100

    ADD: base.mk file

--- make-tools/cursor.mk
+++
@@ -1,3 +0,0 @@
-define hidden_cursor
-	@echo -ne "\x1b[?25l"
-endef
--- make-tools/project-config.mk
+++
@@ -1,8 +0,0 @@
-_PROJECT_NAME			=	Makefile
-_PROJECT_VERSION	=	1.0.0
-_INCLUDES_DIR			=	src/includes
-_SRCS_DIR					=	src src/engine src/entities src/map src/window
-_LIBS_DIR					=	-Lsrc/lib/minilibx-linux
-_INCLUDES_FLAGS		=	-Isrc/includes -Isrc/lib/minilibx-linux
-_LIBS_FLAGS				=	
-_CFLAGS						= 
