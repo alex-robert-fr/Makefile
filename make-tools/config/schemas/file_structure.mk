d3ccc666a29a4e46b5b92688349db3432acf9cde
Alex Robert <contact.alexrobert04@gmail.com>
2024-01-03 16:02:00 +0100

    SAVE SPECIAL

--- make-tools/config/schemas/file_structure.mk
+++ make-tools/config/schemas/file_structure.mk
@@ -2,6 +2,9 @@ SECTION_TITLE					=	$(shell printf "$(BOLD)%-106s$(RESET)" "   FILE STRUCTURE
 COLUMN_LEFT_TITLE			=	$(shell printf "$(PRIMARY_ONE)$(BOLD)  $(TITLE_COLOR)%-50s$(RESET)" " Sources Files:")
 COLUMN_RIGHT_TITLE		=	$(shell printf "$(PRIMARY_ONE)$(BOLD)  $(TITLE_COLOR)%-48s$(RESET)" " Includes Files:")
 
+TEXTS1 = text1 text2 text3 text4
+TEXTS2 = textA textB textC textD textE textF
+
 define file_structure
 1
 .	$(SECTION_TITLE),
@@ -10,4 +13,3 @@ define file_structure
 =
 endef
 export file_structure
-
